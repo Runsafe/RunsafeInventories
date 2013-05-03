63d110bbef5bacc39d07323132ed41d2c79e67b7
Kruithne <kruithne@gmail.com>
2013-05-03 01:27:48 +0100

    Added in some quick debug

--- src/no/runsafe/runsafeinventories/UniverseHandler.java
+++ src/no/runsafe/runsafeinventories/UniverseHandler.java
@@ -2,6 +2,7 @@ package no.runsafe.runsafeinventories;
 
 import no.runsafe.framework.configuration.IConfiguration;
 import no.runsafe.framework.event.IConfigurationChanged;
+import no.runsafe.framework.output.IOutput;
 import no.runsafe.framework.server.RunsafeWorld;
 
 import java.util.HashMap;
@@ -10,6 +11,11 @@ import java.util.Map;
 
 public class UniverseHandler implements IConfigurationChanged
 {
+	public UniverseHandler(IOutput output)
+	{
+		this.output = output;
+	}
+
 	public String getUniverseName(String worldName)
 	{
 		if (this.universes.containsKey(worldName))
@@ -31,10 +37,12 @@ public class UniverseHandler implements IConfigurationChanged
 		for (String name : section.keySet())
 		{
 			List<String> worlds = section.get(name);
+			this.output.write(String.format("Universe %s found with %s worlds.", name, worlds.size()));
 			for (String worldName : worlds)
 				this.universes.put(worldName, name);
 		}
 	}
 
 	private HashMap<String, String> universes = new HashMap<String, String>();
+	private IOutput output;
 }
