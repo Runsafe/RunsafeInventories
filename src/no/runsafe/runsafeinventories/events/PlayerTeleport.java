343cfd5825308fcb3569376e8d9611b7fd1c4167
Kruithne <kruithne@gmail.com>
2013-05-03 01:55:42 +0100

    Reverted to use world names rather than universes.

--- src/no/runsafe/runsafeinventories/events/PlayerTeleport.java
+++ src/no/runsafe/runsafeinventories/events/PlayerTeleport.java
@@ -3,27 +3,21 @@ package no.runsafe.runsafeinventories.events;
 import no.runsafe.framework.event.player.IPlayerTeleportEvent;
 import no.runsafe.framework.server.event.player.RunsafePlayerTeleportEvent;
 import no.runsafe.runsafeinventories.InventoryHandler;
-import no.runsafe.runsafeinventories.UniverseHandler;
 
 public class PlayerTeleport implements IPlayerTeleportEvent
 {
-	public PlayerTeleport(InventoryHandler inventoryHandler, UniverseHandler universeHandler)
+	public PlayerTeleport(InventoryHandler inventoryHandler)
 	{
 		this.inventoryHandler = inventoryHandler;
-		this.universeHandler = universeHandler;
 	}
 
 	@Override
 	public void OnPlayerTeleport(RunsafePlayerTeleportEvent event)
 	{
 		// Check if we're about to teleport to another world.
-		String sourceUniverse = this.universeHandler.getUniverseName(event.getFrom().getWorld());
-		String targetUniverse = this.universeHandler.getUniverseName(event.getTo().getWorld());
-
-		if (!sourceUniverse.equals(targetUniverse))
+		if (!event.getTo().getWorld().getName().equals(event.getFrom().getWorld().getName()))
 			this.inventoryHandler.handlePreWorldChange(event.getPlayer());
 	}
 
 	private InventoryHandler inventoryHandler;
-	private UniverseHandler universeHandler;
 }
