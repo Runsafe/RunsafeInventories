c6e222fb20025c666cfaffa287af193ac3f545ae
Kruithne <kruithne@gmail.com>
2013-05-31 19:49:56 +0100

    Remove any buffs the player has if we change universe

--- src/no/runsafe/runsafeinventories/events/PlayerWorldChange.java
+++ src/no/runsafe/runsafeinventories/events/PlayerWorldChange.java
@@ -2,20 +2,29 @@ package no.runsafe.runsafeinventories.events;
 
 import no.runsafe.framework.event.player.IPlayerChangedWorldEvent;
 import no.runsafe.framework.server.event.player.RunsafePlayerChangedWorldEvent;
+import no.runsafe.framework.server.player.RunsafePlayer;
 import no.runsafe.runsafeinventories.InventoryHandler;
+import no.runsafe.runsafeinventories.UniverseHandler;
 
 public class PlayerWorldChange implements IPlayerChangedWorldEvent
 {
-	public PlayerWorldChange(InventoryHandler inventoryHandler)
+	public PlayerWorldChange(InventoryHandler inventoryHandler, UniverseHandler universeHandler)
 	{
 		this.inventoryHandler = inventoryHandler;
+		this.universeHandler = universeHandler;
 	}
 
 	@Override
 	public void OnPlayerChangedWorld(RunsafePlayerChangedWorldEvent event)
 	{
-		this.inventoryHandler.handlePostWorldChange(event.getPlayer());
+		RunsafePlayer player = event.getPlayer();
+		this.inventoryHandler.handlePostWorldChange(player);
+
+		// Remove any buff effects if we're changing universe
+		if (!universeHandler.getUniverseName(event.getSourceWorld()).equals(universeHandler.getUniverseName(player.getWorld())))
+			player.removeBuffs();
 	}
 
 	private InventoryHandler inventoryHandler;
+	private UniverseHandler universeHandler;
 }
