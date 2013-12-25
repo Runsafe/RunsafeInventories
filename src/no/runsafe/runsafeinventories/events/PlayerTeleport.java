8affc359a55e6af4d6abec30a8ffdb1785d2078d
Morten Nilsen <morten@runsafe.no>
2013-12-25 15:57:18 +0100

    Do null checks on worlds and log warnings if one of the worlds are null. Fixes #2.

--- src/no/runsafe/runsafeinventories/events/PlayerTeleport.java
+++ src/no/runsafe/runsafeinventories/events/PlayerTeleport.java
@@ -4,6 +4,7 @@ import no.runsafe.framework.api.ILocation;
 import no.runsafe.framework.api.IWorld;
 import no.runsafe.framework.api.event.player.IPlayerPortalEvent;
 import no.runsafe.framework.api.event.player.IPlayerTeleportEvent;
+import no.runsafe.framework.api.log.IConsole;
 import no.runsafe.framework.api.log.IDebug;
 import no.runsafe.framework.api.player.IPlayer;
 import no.runsafe.framework.minecraft.event.player.RunsafePlayerPortalEvent;
@@ -12,9 +13,10 @@ import no.runsafe.runsafeinventories.InventoryHandler;
 
 public class PlayerTeleport implements IPlayerTeleportEvent, IPlayerPortalEvent
 {
-	public PlayerTeleport(InventoryHandler inventoryHandler, IDebug output)
+	public PlayerTeleport(InventoryHandler inventoryHandler, IConsole console, IDebug output)
 	{
 		this.inventoryHandler = inventoryHandler;
+		this.console = console;
 		this.debugger = output;
 	}
 
@@ -41,12 +43,24 @@ public class PlayerTeleport implements IPlayerTeleportEvent, IPlayerPortalEvent
 
 	private void checkTeleportEvent(IWorld to, IWorld from, IPlayer player)
 	{
-		debugger.debugFine("Going from %s/%s to %s/%s", from.getName(), from.getUniverse().getName(), to.getName(), to.getUniverse().getName());
+		String goingFrom = null;
+		String goingTo = null;
+		if (from != null)
+			goingFrom = String.format("%s/%s", from.getName(), from.getUniverse().getName());
+		else
+			console.logWarning("Player %s portal event from world null!", player.getName());
+
+		if (to != null)
+			goingTo = String.format("%s/%s", to.getName(), to.getUniverse().getName());
+		else
+			console.logWarning("Player %s portal event to world null!", player.getName());
+
+		debugger.debugFine("Going from %s to %s", goingFrom, goingTo);
 		if (to != null && from != null && !to.equals(from))
 			this.inventoryHandler.handlePreWorldChange(player);
 	}
 
 	private final InventoryHandler inventoryHandler;
+	private final IConsole console;
 	private final IDebug debugger;
-
 }
