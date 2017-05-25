3fc936b284167356ef499d38e490f86b71f9374c
njs41 <njs42@hotmail.com>
2017-05-25 15:11:02 -0400

    Make user confirm whether or not to delete an inventory region.

--- src/no/runsafe/runsafeinventories/commands/RemoveRegion.java
+++ src/no/runsafe/runsafeinventories/commands/RemoveRegion.java
@@ -1,5 +1,6 @@
 package no.runsafe.runsafeinventories.commands;
 
+import no.runsafe.framework.api.IScheduler;
 import no.runsafe.framework.api.IWorld;
 import no.runsafe.framework.api.command.ExecutableCommand;
 import no.runsafe.framework.api.command.ICommandExecutor;
@@ -8,9 +9,11 @@ import no.runsafe.framework.api.command.argument.RequiredArgument;
 import no.runsafe.framework.api.command.argument.WorldArgument;
 import no.runsafe.runsafeinventories.RegionInventoryHandler;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 public class RemoveRegion extends ExecutableCommand
 {
-	public RemoveRegion(RegionInventoryHandler regionInventoryHandler)
+	public RemoveRegion(RegionInventoryHandler regionInventoryHandler, IScheduler scheduler)
 	{
 		super("removeregion",
 			"Removes an inventory region and wipes its inventory data.",
@@ -19,6 +22,7 @@ public class RemoveRegion extends ExecutableCommand
 			new RequiredArgument(REGION)
 		);
 		this.regionInventoryHandler = regionInventoryHandler;
+		this.scheduler = scheduler;
 	}
 
 	private static final String WORLD = "world";
@@ -27,10 +31,47 @@ public class RemoveRegion extends ExecutableCommand
 	@Override
 	public String OnExecute(ICommandExecutor executor, IArgumentList parameters)
 	{
-		if (regionInventoryHandler.removeInventoryRegion(((IWorld) parameters.getValue(WORLD)).getName(), parameters.getValue(REGION)))
-			return "Inventory region removed.";
-		return "&cInventory region could not be removed.";
+		IWorld world = parameters.getValue(WORLD);
+		if (world == null)
+			return ("&cInvalid world.");
+
+		String worldName = world.getName();
+		String regionName = parameters.getValue(REGION);
+
+		if (wipers.containsKey(executor)) // If the user is executing the command again, confirm deletion.
+		{
+			scheduler.cancelTask(wipers.get(executor));
+			wipers.remove(executor);
+			if (regionInventoryHandler.removeInventoryRegion(worldName, regionName))
+				return String.format("Inventory region %s removed.", regionName);
+			return String.format("&cInventory region %s could not be removed.", regionName);
+		}
+		else
+		{
+			wipers.put(executor, scheduler.startSyncTask(new Runnable()
+			{
+				@Override
+				public void run()
+				{
+					if (wipers.containsKey(executor))
+					{
+						executor.sendColouredMessage("&cCancelling remove inventory region attempt.");
+						wipers.remove(executor);
+					}
+				}
+			}, 15));
+
+			executor.sendMessage(
+				String.format(
+					"Are you sure you want to delete the inventory region %s in %s? This cannot be undone.",
+					regionName, worldName
+				)
+			);
+			return "Run the command again to confirm inventory region deletion.";
+		}
 	}
 
+	private final IScheduler scheduler;
+	private final ConcurrentHashMap<ICommandExecutor, Integer> wipers = new ConcurrentHashMap<ICommandExecutor, Integer>();
 	private final RegionInventoryHandler regionInventoryHandler;
 }
